b30361273509ac4157c1b559485f418557aa5b94
seungshins <seungshins@cl0.beta.service.aws.koding.com>
2013-05-19 22:54:10 -0700

    kkkkk

---
+++ apitest.js
@@ -0,0 +1,66 @@
+/**
+ * CRUD API Test Server Script
+ * version : v.1.0
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require("http")
+  , fs= require('fs')
+  , ejs = require('ejs');
+
+//var app = module.exports = express.createServer();
+var app = express();
+
+var RestHome = require('./webroot/rest/home');
+
+//mongodb setup
+var databaseUrl = "dbuser:1234@linus.mongohq.com:10030/TB"; //  "username:password@example.com/mydb"
+var db = require('mongoskin').db(databaseUrl); 
+
+app.enable("jsonp callback");
+
+// Configuration
+app.configure(function(){
+  app.engine('.html', require('ejs').__express);
+  app.set('port',process.env.C9_PORT || 8001);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'html');
+  app.use( express.favicon());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.logger()); 
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+  app.set("jsonp callback", true);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+
+// Routes
+app.get('/', function(req, res){
+    var userInfo;
+    db.collection('TB_USER').find().toArray(function(err, result) {
+        if (err) throw err;
+        userInfo = result; 
+        console.log(result);
+        res.render('TestPage', {pageTitle: 'CRUD Demo',userInfo: userInfo});
+    });
+}); 
+
+//Api handling
+app.get('/rest/:apiname', RestHome.process);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+});
+
+//app.listen(process.env.C9_PORT || 8001);
+//console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
